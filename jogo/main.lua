9ef8f6a72041d2dcaafd1981caf86a7963150335
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2019-08-31 11:59:21 -0300

    linha de comando

--- jogo/main.lua
+++ jogo/main.lua
@@ -2,11 +2,8 @@ require "map"
 require "camera"
 
 function love.load(path)
-	for i, j in pairs(path) do
-		print(i, j)
-	end
---	local chunck = love.filesystem.load(path)
---	local result = chunk()
+	local chunk = love.filesystem.load("maps/"..path[1]..".lua")
+	local result = chunk()
 end
 
 function love.update(dt)
